da362af383f08b27456946cca44ba5915597f084
jvalde17 <jvalde17@asu.edu>
2020-01-31 08:22:08 -0800

    updated login.py to support recall of last page visited

--- html/cgi_bin/login.py
+++ html/cgi_bin/login.py
@@ -14,12 +14,13 @@ print
 print '<h1>Welcome to SolarSPELL </h1>'
 print '<form action = "save_user.py" method =  "post">'
 print '<input type = "text" name = "userid" >'
-print '<input type="submit" value = "Go to Main" >'
+print '<input type="submit" value = "Go to VoA" >'
+print '<input type="submit" value = "Go to last visited lesson" formaction = "recall_last_visited_page.py" >'
 print '</form>'
 
-#replace this with read_last_visited.py
+
 #with samilar functionality as save_user.py
-print '<form action = "\content/Language Arts/Voice of America (VOA)/VOA_Landing_Page.html" method =  "post">'
-print '<input type="submit" value = "Go to last visited lesson">'
-print '</form>'
+#print '<form action = "recall_last_visited_page.py" method =  "post">'
+#print '<input type="submit" value = "Go to last visited lesson">'
+#print '</form>'
 
